0f68a9354edb6f8b0924a9814e2b714f30ae987f
Marcel <marcel.namyslo@student.kit.edu>
2022-06-19 16:10:27 +0200

    new mouse movement function

--- script.js
+++ script.js
@@ -22,6 +22,105 @@ var Keys = {
     right: false
 };
 
+
+function moveRocket2() {
+  document.addEventListener("mousemove", function(e){
+    let rocket = document.querySelector('.rocket');
+    let flame = document.querySelector('.flame');
+    rocket.style.left= e.offsetX - 35  + 'px';
+    rocket.style.top = e.offsetY -55 + 'px';
+    flame.style.left= e.offsetX -20  + 'px';
+    flame.style.top = e.offsetY -3 + 'px';
+    var up = parseInt(window.getComputedStyle(rocket).getPropertyValue("top"));
+    var left = parseInt(window.getComputedStyle(rocket).getPropertyValue("left"));
+
+    
+
+    
+   
+  })
+}
+
+function shoot() {
+  let rocket = document.querySelector('.rocket');
+  var up = parseInt(window.getComputedStyle(rocket).getPropertyValue("top"));
+  var left = parseInt(window.getComputedStyle(rocket).getPropertyValue("left"));
+
+window.addEventListener("keydown", (e) => {
+   if (e.keyCode  == 1 || e.keyCode == 32) {
+      console.log(rocket);
+      let screen = document.querySelector('.screen');
+      //32 is for space key
+      //flame.style.left = left;
+      //flame.style.up = up;
+      var shot = document.createElement("div");
+      
+      shot.classList.add("shot");
+      screen.appendChild(shot);
+      
+      var bottom = window.innerHeight - up- 230 ;
+      shot.style.bottom = bottom +  "px";
+  
+      var moveshot= setInterval(() => {
+          var rocks = document.getElementsByClassName("rock");
+          for (var i = 0; i < rocks.length; i++) {
+              var rock = rocks[i];
+              if (rock != undefined) {
+                  var rockbound = rock.getBoundingClientRect();
+                  var blastbound = shot.getBoundingClientRect();
+      
+                //Condition to check whether the rock/alien and the bullet are at the same position..!
+                //If so,then we have to destroy that roc
+                if (
+                  blastbound.left >= rockbound.left -20 &&
+                  blastbound.right <= rockbound.right +60  &&
+                  
+                  blastbound.bottom <= rockbound.bottom -100
+                ) {
+                  rock.parentElement.removeChild(rock); 
+                  shot.parentElement.removeChild(shot);
+                  
+                }
+              }
+            }
+        var shotbottom = parseInt(
+          window.getComputedStyle(shot).getPropertyValue("bottom")
+        );
+       
+        //Stops the shot from moving outside the gamebox
+        if (shotbottom >= 1000) {
+          clearInterval(moveshot);
+        }
+  
+        shot.style.left = parseInt(window.getComputedStyle(rocket).getPropertyValue("left")) + 22 +  "px"; //shot should always be placed at the top of my jet..!
+       
+        shot.style.bottom = parseInt(window.getComputedStyle(shot).getPropertyValue("bottom")) + 7  + "px";
+      });
+    }
+  });
+
+  document.addEventListener("mousemove", function(e){
+    let rocket = document.querySelector('.rocket');
+    let flame = document.querySelector('.flame');
+    rocket.style.left= e.offsetX - 35  + 'px';
+    rocket.style.top = e.offsetY -55 + 'px';
+    flame.style.left= e.offsetX -20  + 'px';
+    flame.style.top = e.offsetY -3 + 'px';
+    var up = parseInt(window.getComputedStyle(rocket).getPropertyValue("top"));
+    var left = parseInt(window.getComputedStyle(rocket).getPropertyValue("left"));
+
+    
+
+    
+   
+  })
+  
+}
+  
+
+
+
+
 function moveRocket() {
 
 
@@ -81,7 +180,7 @@ else if (Keys.right && right > 0) {
 }
 
 
-if (e.key == "ArrowUp" || e.keyCode == 32) {
+if (e.key == "ArrowUp" || e.keyCode == 32 ) {
     let screen = document.querySelector('.screen');
     //32 is for space key
     //flame.style.left = left;
@@ -116,9 +215,7 @@ if (e.key == "ArrowUp" || e.keyCode == 32) {
               }
             }
           }
-      var shotbottom = parseInt(
-        window.getComputedStyle(shot).getPropertyValue("bottom")
-      );
+      var shotbottom = parseInt(window.getComputedStyle(shot).getPropertyValue("bottom"));
      
       //Stops the shot from moving outside the gamebox
       if (shotbottom >= 1000) {
@@ -215,10 +312,10 @@ function stars(){
       };
     
     },14)
-    var deleteStars = setInterval(() => {
-      screen.removeChild(star)
+    //var deleteStars = setInterval(() => {
+    // screen.removeChild(star)
       
-      },14)
+     // },14)
 }
 
    
@@ -304,5 +401,7 @@ DeviceMotionEvent.requestPermission().then(response => {
    stars();
    rocks();
    moveRocket();
+   //shoot();
+   //moveRocket2();
 
-
+  
