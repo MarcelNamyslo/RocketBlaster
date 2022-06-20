8a5fee83f07b1bde26f732955c3a943b5fa81f27
Marcel <marcel.namyslo@student.kit.edu>
2022-06-20 17:35:22 +0200

    change viewport

--- script.js
+++ script.js
@@ -402,6 +402,11 @@ DeviceMotionEvent.requestPermission().then(response => {
         });
     }
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
    stars();
    rocks();
