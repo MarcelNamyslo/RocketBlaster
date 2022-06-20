540393f79f14ff860f4e355bbf38dd2ec6cc9cc7
Marcel <marcel.namyslo@student.kit.edu>
2022-06-20 16:41:46 +0200

    change movement speed on devide

--- script.js
+++ script.js
@@ -376,19 +376,19 @@ DeviceMotionEvent.requestPermission().then(response => {
             
             // Update velocity according to how tilted the phone is
             // Since phones are narrower than they are long, double the increase to the x velocity
-            vx = vx + leftToRight_degrees * updateRate*2; 
-            vy = vy + frontToBack_degrees * updateRate;
+            vx = vx + leftToRight_degrees * updateRate*4/3; 
+            vy = vy + frontToBack_degrees * updateRate *2/3;
             
             // Update position and clip it to bounds
             px = px + vx*.5;
             if (px > 98 || px < 0){ 
-                px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+                px = Math.max(0, Math.min(70, px)) // Clip px between 0-98
                 vx = 0;
             }
 
             py = py + vy*.5;
             if (py > 98 || py < 0){
-                py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+                py = Math.max(0, Math.min(70, py)) // Clip py between 0-98
                 vy = 0;
             }
             rocket = document.querySelector('.rocket');
