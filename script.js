81b9980320b03c466f0ad917592f081b4630ceca
Marcel <marcel.namyslo@student.kit.edu>
2022-06-20 17:13:37 +0200

    flame top

--- script.js
+++ script.js
@@ -397,7 +397,7 @@ DeviceMotionEvent.requestPermission().then(response => {
             rocket.setAttribute('style', "left:" + (px) + "%;" +
                                           "top:" + (py) + "%;");
             flame.setAttribute('style', "left:" + (px+2) + "%;" +
-            "top:" + (py+3) + "%;");
+            "top:" + (py+5) + "%;");
             
         });
     }
