0ab51f4a26f689fe9e708b5aa33c2e456a3a459b
Marcel <marcel.namyslo@student.kit.edu>
2022-07-25 17:30:17 +0200

    unnnio

--- script.js
+++ script.js
@@ -350,7 +350,7 @@ function rocks() {
             }
           } 
       } else {
-          let x = Math.floor(Math.random() * window.innerWidth )-100;
+          let x = Math.floor(Math.random() * window.innerWidth )-200;
          
           if(x > 0 && x < (width *0.8) ) {
 
