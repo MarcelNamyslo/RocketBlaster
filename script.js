e7ad1f18e62f03dfda487781c378c6776c8f0464
Marcel <marcel.namyslo@student.kit.edu>
2022-09-21 13:22:23 +0200

    ihhb

--- script.js
+++ script.js
@@ -558,7 +558,7 @@ function moveonmobile(event) {
                 if (
                   rocketbound.left >= rockbound.left   &&
                   rocketbound.left <= rockbound.left + 50  &&
-                  rocketbound.top -120 <= rockbound.top   
+                  rocketbound.top  <= rockbound.top   
                   //rocketbound.top  >= rockbound.top -80  ) {
                 ){
                   stoprunning();
